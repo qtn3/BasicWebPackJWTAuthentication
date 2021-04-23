511a619a232aba41f79aa248d9784ba1a96b5fb7
qnguy <qtn3@njit.edu>
2021-04-23 17:24:55 -0500

    Created an array of users

---
+++ server/auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
+});
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin',
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member',
+    },
+];
+
+app.use(bodyParser.json());
